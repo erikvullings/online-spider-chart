0b0ef65a1f849a2e4b288bcdae715d1c754b99ef
Erik Vullings <erik.vullings@gmail.com>
2021-06-15 23:44:03 +0200

    Redirect build to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env.production;
-  const outputPath = path.resolve(__dirname, isProduction ? '../../docs' : 'dist');
+  const outputPath = path.resolve(__dirname, isProduction ? './docs' : 'dist');
   const publicPath = isProduction ? 'https://erikvullings.github.io/online-spider-chart/' : '/';
 
   console.log(
